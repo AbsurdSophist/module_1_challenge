3644c95794f0e3d72be58c7967e9c0318413bd7e
Lucas Manning <AbsurdSophist@gmail.com>
2020-12-27 17:05:06 -0800

    present value calculated, current cost contrasted

--- main.py
+++ main.py
@@ -6,41 +6,17 @@ loan_costs = [500, 600, 200, 1000, 450]
 
 #number of loans calculated and printed
 number_of_loans = len(loan_costs)
-print(number_of_loans)
+print(f"Number of loans: {number_of_loans}")
 
 #sum of loans calculated and printed
 loan_totals = sum(loan_costs)
-print(loan_totals)
+print(f"Total amount of all loans: ${loan_totals: .2f}")
 
 #average loan amount calculated and printed
 average_loan_amount = loan_totals / number_of_loans
-print(average_loan_amount)
+print(f"Average loan amount: ${average_loan_amount: .2f}")
 
-
-"""Part 2: Analyze Loan Data.
-
-Analyze the loan to determine the investment evaluation.
-
-Using more detailed data on one of these loans, follow these steps to calculate a Present Value, or a "fair price" for what this loan would be worth.
-
-1. Use get() on the dictionary of additional information to extract the **Future Value** and **Remaining Months** on the loan.
-    a. Save these values as variables called `future_value` and `remaining_months`.
-    b. Print each variable.
-
-    @NOTE:
-    **Future Value**: The amount of money the borrower has to pay back upon maturity of the loan (a.k.a. "Face Value")
-    **Remaining Months**: The remaining maturity (in months) before the loan needs to be fully repaid.
-
-2. Use the formula for Present Value to calculate a "fair value" of the loan. Use a minimum required return of 20% as the discount rate.
-3. Write a conditional statement (an if-else statement) to decide if the present value represents the loan's fair value.
-    a. If the present value of the loan is greater than or equal to the cost, then print a message that says the loan is worth at least the cost to buy it.
-    b. Else, the present value of the loan is less than the loan cost, then print a message that says that the loan is too expensive and not worth the price.
-
-    @NOTE:
-    If Present Value represents the loan's fair value (given the required minimum return of 20%), does it make sense to buy the loan at its current cost?
-"""
-
-# Given the following loan data, you will need to calculate the present value for the loan
+#given loan data
 loan = {
     "loan_price": 500,
     "remaining_months": 9,
@@ -48,23 +24,20 @@ loan = {
     "future_value": 1000,
 }
 
-# @TODO: Use get() on the dictionary of additional information to extract the Future Value and Remaining Months on the loan.
-# Print each variable.
-# YOUR CODE HERE!
+# future value and remaining months on the loan obtained from given loan data, results printed
+future_value = loan.get("future_value")
+remaining_months = loan.get("remaining_months")
+print(future_value)
+print(remaining_months)
 
+# calculation of present value using given loan data
+present_value = future_value / (1 + .20/12) ** remaining_months
 
-# @TODO: Use the formula for Present Value to calculate a "fair value" of the loan.
-# Use a minimum required return of 20% as the discount rate.
-#   You'll want to use the **monthly** version of the present value formula.
-#   HINT: Present Value = Future Value / (1 + Discount_Rate/12) ** remaining_months
-
-# YOUR CODE HERE!
-
-# If Present Value represents what the loan is really worth, does it make sense to buy the loan at its cost?
-# @TODO: Write a conditional statement (an if-else statement) to decide if the present value represents the loan's fair value.
-#    If the present value of the loan is greater than or equal to the cost, then print a message that says the loan is worth at least the cost to buy it.
-#    Else, the present value of the loan is less than the loan cost, then print a message that says that the loan is too expensive and not worth the price.
-# YOUR CODE HERE!
+# buyer recommendation based on loan's present value with respect to loan's current cost, results printed
+if present_value >= loan["loan_price"]:
+    print("The loan is worth buying at current cost")
+else:
+    print("The loan is too expensive and not worth the price")
 
 
 """Part 3: Perform Financial Calculations.
